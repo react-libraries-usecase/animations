74f93c291a2748b833ed1071180d2f4d8e0c3049
jafarRezaei <bomber.man87@yahoo.com>
2019-10-13 23:07:13 +0330

    add notes part and fix story book bugs

--- packages/CssTransition/stories/index.story.js
+++ packages/CssTransition/stories/index.story.js
@@ -1,47 +1,9 @@
 import React, { lazy, Suspense } from 'react';
 import { storiesOf } from '@storybook/react';
 
-const text = `
-# Title
+// import notes
+import BasicNote from './../notes/Basic';
 
-<Giphy gif='cheese' />
-
-~~~js
-import React, { useState } from 'react';
-import './style.css';
-
-const Basic = () => {
-  const [infinite, setInfinite] = useState(false);
-  const [playing, setPlaying] = useState(false);
-
-  const handleInfiniteChange = e => {
-    setInfinite(e.target.checked);
-  };
-  const handlePlayingChange = () => {
-    setPlaying(!playing);
-  };
-
-  return (
-    <>
-      <div
-        className={
-          'wrapper ' + (infinite && ' infinitePlay ') + (playing && 'playing')
-        }
-      >
-        <div className="object" />
-      </div>
-      <label>
-        Infinite:
-        <input type="checkbox" onChange={handleInfiniteChange} />
-      </label>
-      <input type="button" value="Play" onChange={handlePlayingChange} />
-    </>
-  );
-};
-
-export default Basic;
-~~~
-`;
 storiesOf('CssTransition', module)
   .add('Basic', () => {
     const Basic = lazy(() => import('./../src/Basic/index'));
@@ -51,5 +13,5 @@ storiesOf('CssTransition', module)
       </Suspense>
     );
   },{
-    notes: text ,
+    notes: BasicNote ,
   });
